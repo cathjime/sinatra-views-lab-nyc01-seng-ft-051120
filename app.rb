e09c93508cae4e6ca93172af1384223a43b81cd9
cathjime <xcatherine.jimenez@gmail.com>
2020-06-01 18:42:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,25 @@
+require 'date'
+
 class App < Sinatra::Base
+
 
 	get '/' do
 		erb :index
 	end
+
+	get '/hello' do
+		erb :index
+	end
+
+	get '/goodbye' do
+		erb :index
+	end
+
+	get '/date' do
+		erb :date
+	end
+
+	
 
 
 end
